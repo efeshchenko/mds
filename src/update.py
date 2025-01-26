85772a3bcadd56238f919986c0721847cdbecb9d
Eugene Feshchenko <eugene.feshchenko@meridia.land>
2025-01-26 22:51:43 +0100

    Upd db

--- src/update.py
+++ src/update.py
@@ -36,8 +36,6 @@ class Crawler:
         return names
 
     def write_scrapped_info(self):
-        print("Writing scrapped info...")
-
         with sqlite3.connect("mds.db") as conn:
             cursor = conn.cursor()
             cursor.executemany(
@@ -48,14 +46,13 @@ class Crawler:
                 [(x.name, x.author, x.links, "pending") for x in self.scrapped],
             )
             conn.commit()
+        print("\n")
         print(f"Wrote {len(self.scrapped)} new items to database")
+        print("\n")
 
     async def download_page(self, url: str) -> str:
         async with self.semaphore:
-            try:
-                response = await self.client.get(url)
-            finally:
-                pass#await self.client.aclose()
+            response = await self.client.get(url)
         return response.text
 
     async def scrap_book(self, name: str, author: str, url: str) -> None:
@@ -63,6 +60,21 @@ class Crawler:
         soup = BeautifulSoup(response, "html.parser")
         links = "|".join([x["href"] for x in soup.find_all("a") if x["href"].endswith(".mp3")])
         self.scrapped.append(ParsedAudioBook(name=name, author=author, links=links))
+
+    def find_new_books(self, soup: BeautifulSoup) -> list:
+        result = []
+        table = soup.find(id="catalogtable")
+        for row in table.find_all("tr")[1:]:
+            cells = row.find_all("td")
+            if not cells[2].find("a"):
+                continue
+            name = cells[2].find("a").text
+            author = cells[1].find("a").text.replace("\xa0", " ")
+            url = cells[2].find("a")["href"]
+            if not name in self.known_book_names:
+                result.append((name, author, url))
+
+        return result
 
     async def crawl(self) -> None:
         if not self.URLS_PENDING:
@@ -78,18 +90,8 @@ class Crawler:
         self.URLS_PENDING += [x for x in links if x not in self.URLS_PROCESSED + self.URLS_PENDING]
         self.URLS_PENDING = list(set(self.URLS_PENDING))
 
-        tasks = [self.crawl() for _ in range(len(self.URLS_PENDING))]
-
-        table = soup.find(id="catalogtable")
-        for row in table.find_all("tr")[1:]:
-            cells = row.find_all("td")
-            if not cells[2].find("a"):
-                continue
-            name = cells[2].find("a").text
-            author = cells[1].find("a").text.replace("\xa0", " ")
-            if not name in self.known_book_names:
-                tasks.append(self.scrap_book(name=name, author=author, url=cells[2].find("a")["href"]))
-
+        tasks = [self.scrap_book(name, author, detail_url) for name, author, detail_url in self.find_new_books(soup)]
+        tasks += [self.crawl() for _ in range(len(self.URLS_PENDING))]
         await asyncio.gather(*tasks)
 
 
